f7f64abc57bd0fbe96e6b77f0e244182376d4160
Joseds160899 <jose.ds.160899@gmail.com>
2022-10-10 18:04:02 -0500

    seeder- jose

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Controllers\ProductController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
