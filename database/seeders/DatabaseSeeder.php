0d7c4c532679de22c04a56e8813d3fafbee05e6d
jolumasa <jorgeluis04ms02@gmail.com>
2022-10-11 11:28:33 -0500

    migrations solicitud compra - jorge

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\SolicitudCompra;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +21,7 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345678'),
         ]);
 
+        SolicitudCompra::factory(20)->create();
 
     }
 }
