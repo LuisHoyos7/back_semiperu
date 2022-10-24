547ecefaf7f0df344faf89f691c86828a98ab556
Joseds160899 <jose.ds.160899@gmail.com>
2022-10-24 17:56:21 -0500

    creacion de permisos y roles proveedor - jose

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,98 +16,55 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
+        // -------------------- ROLES - USUARIOS ------------------------//
+        $adminRole                      = Role::create(['name'=>'Admin']);
+        $PersonalRole                   = Role::create(['name'=>'Personal']);
+        $ProjectRole                    = Role::create(['name'=>'Project']);
+        $ControllerRole                 = Role::create(['name'=>'Controller']);
+        $GerenteOperacionesRole         = Role::create(['name'=>'Manager']);
+        $ContadoraRole                  = Role::create(['name'=>'Accountant']);
+        $proveedorRole                  = Role::create(['name'=>'Supplier']);
+        // ------------------------------------------------------------------//
 
-        $adminRole = Role::create(['name'=>'Admin']);
-        $PersonalRole = Role::create(['name'=>'Personal']);
-        $ProjectRole = Role::create(['name'=>'Project']);
-        $ControllerRole = Role::create(['name'=>'Controller']);
-        $GerenteOperacionesRole = Role::create(['name'=>'Manager']);
-        $ContadoraRole = Role::create(['name'=>'Accountant']);
+        // --------------------- PERMISOS - ORDEN DE COMPRA  ---------------------- //
+        // - PROVEEDOR
+        $uploadFilePurchaseOrder        = Permission::create(['name'=> 'Upload File Purchase Order']);
+        $updateFilePurchaseOrder        = Permission::create(['name'=> 'Update File Purchase Order']);
+        $createRecordOfConformity       = Permission::create(['name'=> 'Create Record Conformity']);
+        $createProofOfPayment           = Permission::create(['name'=> 'Create Proof Payment']);
+        // ------------------------------------------------------------- //
+        $proveedorRole->givePermissionTo($uploadFilePurchaseOrder);
+        $proveedorRole->givePermissionTo($updateFilePurchaseOrder);
+        $proveedorRole->givePermissionTo($createRecordOfConformity);
+        $proveedorRole->givePermissionTo($createProofOfPayment);
 
-        //reportes
-        // $reportSolicitudPermission = Spatie\Permission\Models\Permission::create(['name' => 'Report', 'display_name'=>'Reportes']);
-        // $reportRendicionPermission = Spatie\Permission\Models\Permission::create(['name' => 'Report Reembolso', 'display_name'=>'Reportes Reembolso']);
-        $reportSolicitudPermission = Permission::create(['name' => 'Report']);
-        $ControllerRole->givePermissionTo($reportSolicitudPermission);
-        // Permisos de solicitud
-        $viewSolicitudPermission = Permission::create(['name' => 'View Request']);
-        $ProjectRole->givePermissionTo($viewSolicitudPermission);
-        $ControllerRole->givePermissionTo($viewSolicitudPermission);
-        $GerenteOperacionesRole->givePermissionTo($viewSolicitudPermission);
-        $createSolicitudPermission = Permission::create(['name' => 'Create Request']);
-
-        $ProjectRole->givePermissionTo($createSolicitudPermission);
-        $ControllerRole->givePermissionTo($createSolicitudPermission);
-        $GerenteOperacionesRole->givePermissionTo($createSolicitudPermission);
-        $updateSolicitudPermission = Permission::create(['name' => 'Update Request']);
-
-        $ControllerRole->givePermissionTo($updateSolicitudPermission);
-        $downloadSolicitudPermission = Permission::create(['name' => 'Download Request']);
-
-        $ControllerRole->givePermissionTo($downloadSolicitudPermission);
-        $GerenteOperacionesRole->givePermissionTo($downloadSolicitudPermission);
-
-        $SpecialSolicitudPermission = Permission::create(['name' => 'Special Request']);
-        $ControllerRole->givePermissionTo($SpecialSolicitudPermission);
-
-        // Permisos de Rendición
-        $viewRendicionPermission = Permission::create(['name' => 'View Surrender']);
-        $ProjectRole->givePermissionTo($viewRendicionPermission);
-        $ControllerRole->givePermissionTo($viewRendicionPermission);
-        $GerenteOperacionesRole->givePermissionTo($viewRendicionPermission);
-        $ContadoraRole->givePermissionTo($viewRendicionPermission);
-
-        $createRendicionPermission = Permission::create(['name' => 'Create Surrender']);
-
-        $PersonalRole->givePermissionTo($createRendicionPermission);
-        $ProjectRole->givePermissionTo($createRendicionPermission);
-        $ControllerRole->givePermissionTo($createRendicionPermission);
-        $GerenteOperacionesRole->givePermissionTo($createRendicionPermission);
-
-        $updateRendicionPermission = Permission::create(['name' => 'Update Surrender']);
-
-        $ControllerRole->givePermissionTo($updateRendicionPermission);
-        $GerenteOperacionesRole->givePermissionTo($updateRendicionPermission);
-
-        $downloadRendicionPermission = Permission::create(['name' => 'Download Surrender']);
-
-        $ControllerRole->givePermissionTo($downloadRendicionPermission);
-        $GerenteOperacionesRole->givePermissionTo($downloadRendicionPermission);
-
-        $PaymentsRendicionPermission = Permission::create(['name' => 'Payments Surrender']);
-
-        $specialRendicionPermission = Permission::create(['name' => 'Special Surrender']);
-        // $ControllerRole->givePermissionTo($PaymentsRendicionPermission);
-        $ControllerRole->givePermissionTo($specialRendicionPermission);
-        $ControllerRole->givePermissionTo($PaymentsRendicionPermission);
-        $ContadoraRole->givePermissionTo($PaymentsRendicionPermission);
-
-        $mergeRendicionPermission = Permission::create(['name' => 'Merge Surrender']);
-
-        $ControllerRole->givePermissionTo($mergeRendicionPermission);
-        $ContadoraRole->givePermissionTo($mergeRendicionPermission);
-
-        $viewUsersPermission = Permission::create(['name' => 'View users']);
-        $createUsersPermission = Permission::create(['name' => 'Create users']);
-        $updateUsersPermission = Permission::create(['name' => 'Update users']);
-        $deleteUsersPermission = Permission::create(['name' => 'Delete users']);
+        // -------------------------- PERMISOS ADICIONALES ---------------------------- //
+        $viewUsersPermission    = Permission::create(['name' => 'View users']);
+        $createUsersPermission  = Permission::create(['name' => 'Create users']);
+        $updateUsersPermission  = Permission::create(['name' => 'Update users']);
+        $deleteUsersPermission  = Permission::create(['name' => 'Delete users']);
         $specialUsersPermission = Permission::create(['name' => 'Special users']);
 
-        $viewRolesPermission = Permission::create(['name' => 'View roles']);
-        $createRolesPermission = Permission::create(['name' => 'Create roles']);
-        $updateRolesPermission = Permission::create(['name' => 'Update roles']);
-        $deleteRolesPermission = Permission::create(['name' => 'Delete roles']);
+        $viewRolesPermission    = Permission::create(['name' => 'View roles']);
+        $createRolesPermission  = Permission::create(['name' => 'Create roles']);
+        $updateRolesPermission  = Permission::create(['name' => 'Update roles']);
+        $deleteRolesPermission  = Permission::create(['name' => 'Delete roles']);
 
+        $viewPermission         = Permission::create(['name' => 'View permissions']);
+        $updatePermission       = Permission::create(['name' => 'Update permissions']);
 
-        $viewPermission = Permission::create(['name' => 'View permissions']);
-        $updatePermission = Permission::create(['name' => 'Update permissions']);
+        // -----------------------------------------------------------------------------------//
 
-        // para actualizar reembolso
-        // $actualizarRendicionReembolsoPermission = Spatie\Permission\Models\Permission::create(['name' => 'Update Refund']);
+        // --------------------------- CREACION USUARIOS ---------------------------------- //
+        $proveedor = new User();
+        $proveedor->name = 'Proveedor 1';
+        $proveedor->email = 'proveedor@portalsemiperu.com';
+        $proveedor->password = bcrypt('12345');
+        $proveedor->save();
 
+        $proveedor->assignRole($proveedorRole);
 
         $admin = new User();
-
         $admin->name = 'Administrador';
         $admin->email = 'admin@portalsemiperu.com';
         $admin->password = bcrypt('12345');
@@ -187,15 +144,5 @@ class RoleSeeder extends Seeder
         $manager->save();
 
         $manager->assignRole($ContadoraRole);
-
-        // Storage::disk('public')->deleteDirectory('rendicion');
-        // Storage::disk('public')->deleteDirectory('users');
-        // Storage::disk('public')->deleteDirectory('generatedPdf');
-        // Storage::disk('public')->deleteDirectory('merge');
-        // Storage::disk('public')->deleteDirectory('import');
-
-        // Storage::disk('public')->makeDirectory('generatedPdf');
-        // Storage::disk('public')->makeDirectory('merge');
-        // Storage::disk('public')->makeDirectory('import');
     }
 }
